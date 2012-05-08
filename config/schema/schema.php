1fef0b9197539f2aa40028de4c63f0b1cd4a153a
Richard Kersey <github@razorit.com>
2012-05-08 00:23:12 -0400

    Updated schema files throughout

--- config/schema/schema.php
+++ config/schema/schema.php
@@ -1,74 +1,34 @@
-<?php
-/**
- * CakePHP Ratings
- *
- * Copyright 2009 - 2010, Cake Development Corporation
- *                        1785 E. Sahara Avenue, Suite 490-423
- *                        Las Vegas, Nevada 89104
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright 2009 - 2010, Cake Development Corporation
- * @link      http://github.com/CakeDC/Ratings
- * @package   plugsin.ratings
- * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
+<?php 
+class RatingsSchema extends CakeSchema {
 
-/**
- * Short description for class.
- *
- * @package		ratings
- * @subpackage	config.schema
- */
+	public $renames = array();
 
-class AppSchema extends CakeSchema {
+	public function __construct($options = array()) {
+		parent::__construct();
+	}
 
-/**
- * Schema name
- *
- * @var string
- * @access public
- */
-	public $name = 'App';
-
-/**
- * Before callback
- *
- * @return boolean
- * @access public
- */
 	public function before($event = array()) {
-		return true;
+		App::uses('UpdateSchema', 'Model'); 
+		$this->UpdateSchema = new UpdateSchema;
+		$before = $this->UpdateSchema->before($event);
+		return $before;
 	}
 
-/**
- * After callback
- *
- * @return void
- * @access public
- */
 	public function after($event = array()) {
+		$this->UpdateSchema->rename($event, $this->renames);
+		$this->UpdateSchema->after($event);
 	}
 
-/**
- * Schema for ratings table
- *
- * @var array
- * @access public
- */
 	public $ratings = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
-		'user_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
-		'foreign_key' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
-		'model' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'index'),
-		'value' => array('type' => 'float', 'null' => true, 'default' => '0', 'length' => '8,4'),
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'user_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'foreign_key' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'model' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'value' => array('type' => 'float', 'null' => true, 'default' => '0.0000', 'length' => '8,4'),
+		'comment' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'UNIQUE_RATING' => array('column' => array('user_id', 'foreign_key', 'model'), 'unique' => 1)
-		)
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'UNIQUE_RATING' => array('column' => array('user_id', 'foreign_key', 'model'), 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
 }
-?>
